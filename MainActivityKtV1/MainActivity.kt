540f4c700ab7cf6537ed0f74c358fbe45074e5fb
Dennis Wells <aadennis@users.noreply.github.com>
2022-11-28 22:38:49 +0000

    Add Column and Text

--- MainActivityKtV1/MainActivity.kt
+++ MainActivityKtV1/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.base
+package com.example.mahbasicapp01
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,19 +11,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.base.ui.theme.BaseTheme
+import com.example.mahbasicapp01.ui.theme.MahBasicApp01Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BaseTheme {
+            MahBasicApp01Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Phone")
+                    Postcard("Wish you were", "Jackie O")
                 }
             }
         }
@@ -34,10 +35,18 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    BaseTheme {
-        Greeting("Preview")
+fun aaa() {
+    MahBasicApp01Theme {
+        Postcard("Preview you were", "Jackie O")
+    }
+}
+
+@Composable
+fun Postcard(message: String, from: String) {
+    Column {
+        Text(message)
+        Text(from)
     }
 }
